dc53820fe1d9d77929d60f4d42984eb898509bba
Carlos Henrique <ichfs9108@gmail.com>
2024-01-26 01:00:02 -0300

    Criando o menu de interacao com o usuario

---
+++ screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -0,0 +1,20 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.service.ConsumoAPI;
+
+import java.util.Scanner;
+
+public class Principal {
+
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoAPI consumo = new ConsumoAPI();
+
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=6585022c";
+
+    public void exibeMenu() {
+        System.out.println("Digite o nome da série para busca");
+        var nomeSerie = leitura.nextLine();
+        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+    }
+}
